b86dd5cfa9b84d779a5b572a9ee544cf6dc5f3e7
= <sclarke27@gmail.com>
2022-03-12 15:21:06 -0800

    start adding code for tft support

---
+++ utils/tftPanel/TFTPanel.js
@@ -0,0 +1,160 @@
+const Sprite = require("../../sprites/Sprite");
+const Sprites = require("../../sprites/Sprites");
+const PythonWrapper = require("../pythonWrapper");
+
+const i2c = require("i2c-bus");
+const i2cBus = i2c.openSync(1);
+const oled = require("oled-i2c-bus");
+const font5x7 = require("oled-font-5x7");
+
+class TFTPanel {
+    constructor() {
+        this.ledPixels = [];
+        this.matrix = null;
+        this.isDirty = false;
+        this.tempPixel = null;
+        this.config = {
+            width: 128,
+            height: 64,
+            address: 0x3c,
+        };
+        this.oledBonnet = {
+            file: "./utils/tftPanel/tftPanelBridge.py",
+            process: null,
+        };
+        this.frameWidth = this.config.width * 2;
+        this.showDebug = true;
+        this.start();
+    }
+
+    start() {
+        this.matrix = new PythonWrapper(this.oledBonnet.file, this.showDebug);
+        this.matrix.start();
+        this.matrix.writeMessage(`{"key": "start"}`);
+        // this.matrix = new oled(i2cBus, this.config);
+        // this.matrix.clearDisplay();
+        // this.matrix.turnOnDisplay();
+    }
+
+    clear() {
+        if (this.matrix) {
+            this.matrix.writeMessage(`{"key": "clear"}`);
+            // this.matrix.clearDisplay();
+        }
+    }
+
+    render() {
+        if (this.matrix) {
+            this.matrix.writeMessage(`{"key": "render"}`);
+            // this.matrix.update();
+        }
+    }
+
+    drawText(x, y, text, font, r, g, b) {
+        if (this.matrix) {
+            this.matrix.writeMessage(`{"key": "drawText", "x": ${x}, "y": ${y}, "text": "${text}"}`);
+
+            // this.matrix.setCursor(x, y);
+            // this.matrix.writeString(font5x7, 1, text, 1, false);
+        }
+    }
+
+    drawLine(x0, y0, x1, y1, r, g, b) {
+        if (this.matrix) {
+            const isBlack = r === 0 && g === 0 && b === 0;
+
+            this.matrix.writeMessage(`{"key": "drawLine", "x0": ${x0}, "y0": ${y0}, "x1": "${x1}", "y1": "${y1}", "color": "${isBlack ? 0 : 1}"}`);
+            // this.matrix.drawLine(x0, y0, x1, y1, isBlack ? 0 : 1);
+        }
+    }
+
+    drawCircle(x0, y0, radius, r, g, b) {
+        if (this.matrix) {
+            const isBlack = r === 0 && g === 0 && b === 0;
+            this.matrix.writeMessage(`{"key": "drawCircle", "x": ${x0}, "y": ${y0}, "radius": "${radius}", "color": "${isBlack ? 0 : 1}"}`);
+            // this.matrix.drawCircle(x0, y0, radius, r, g, b);
+        }
+    }
+
+    fill(r, g, b) {
+        if (this.matrix) {
+            // this.matrix.fill(r, g, b);
+        }
+    }
+
+    setPixel(x, y, r, g, b) {
+        if (this.matrix) {
+            const isBlack = r === 0 && g === 0 && b === 0;
+            this.matrix.writeMessage(`{"key": "setPixel", "x": ${x}, "y": ${y}, "color": "${isBlack ? 0 : 1}"}`);
+            // this.matrix.drawPixel([x, y, 1]);
+        }
+    }
+
+    drawSprite(startX, startY, sprite, flipped = false) {
+        const frameWidth = sprite.frameWidth;
+        const frameHeight = sprite.frameHeight;
+        const pallette = sprite.pallette;
+        const frame = sprite.getNextFrame();
+
+        for (let y = 0; y < frameHeight; y++) {
+            const start = y * frameWidth;
+            const end = start + frameWidth;
+            const currentRow = frame.slice(start, end); //frame[y];
+            // console.info(currentRow);
+            if (flipped) {
+                for (let x = frameWidth - 1; x >= 0; x--) {
+                    const currentPixel = currentRow[x];
+                    const currentColor = pallette[currentPixel].split(",");
+                    const r = parseInt(currentColor[0]);
+                    const g = parseInt(currentColor[1]);
+                    const b = parseInt(currentColor[2]);
+                    const isBlack = r === 0 && (g === 0) & (b === 0);
+                    if (!isBlack) {
+                        this.setPixel(startX + (frameWidth - x), startY + y, r, g, b);
+                    }
+                }
+            } else {
+                for (let x = 0; x < frameWidth; x++) {
+                    const currentPixel = currentRow[x];
+                    const currentColor = pallette[currentPixel].split(",");
+                    const r = parseInt(currentColor[0]);
+                    const g = parseInt(currentColor[1]);
+                    const b = parseInt(currentColor[2]);
+                    const isBlack = r === 0 && (g === 0) & (b === 0);
+                    if (!isBlack) {
+                        this.setPixel(startX + x, startY + y, r, g, b);
+                    }
+                }
+            }
+        }
+    }
+
+    drawMountain(x = 0, ground = 0, height = 10, color = [0, 255, 0], borderColor = [150, 150, 0]) {
+        // const mntSprite = new Sprite(Sprites.mountain1)
+        // this.drawSprite(x, ground-height, mntSprite);
+        for (let i = 0; i <= height; i++) {
+            // this.drawLine(x + i, ground - i, x + height * 2 - i, ground - i, ...color);
+        }
+        this.drawMountainLines(x, ground, height, borderColor);
+    }
+
+    drawMountainLines(x = 0, y = 0, height = 10, color = [0, 255, 0]) {
+        let mntStartX = x;
+        let mntStartY = y;
+        let mntHeight = height;
+        let mntWidth = mntHeight * 2;
+        let mntColor = color;
+        let mntTipX = mntStartX + mntWidth / 2;
+        let mntTipY = mntStartY - mntHeight;
+
+        let mntEndX = mntStartX + mntWidth;
+        let mntEndY = mntStartY;
+
+        if (this.matrix) {
+            this.drawLine(mntStartX, mntStartY, mntTipX, mntTipY, ...mntColor);
+            this.drawLine(mntTipX, mntTipY, mntEndX, mntEndY, ...mntColor);
+        }
+    }
+}
+
+module.exports = TFTPanel;
---
+++ utils/tftPanel/tftPanelBridge.py
@@ -0,0 +1,102 @@
+import sys
+import time
+import json
+
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+from PIL import Image, ImageDraw
+import adafruit_ssd1306
+
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
+# Create the SSD1306 OLED class.
+disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c)
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new("1", (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+
+appRunning = True
+showDebug = True
+
+def start():
+    if showDebug:
+      print("[BonnetController] Start Python<->LED Matrix Bridge")
+    sys.stdout.flush()
+    
+    if showDebug:
+      print("[BonnetController] Bridge Ready...")
+
+#start process
+if __name__ == '__main__':
+  start()
+
+  while appRunning:
+    message = input()
+    payload = ""
+    try:
+      payload = json.loads(message)
+      key = payload["key"]
+      
+
+    except:
+
+      print('[BonnetController] json parse error', message)
+    else:
+
+      sys.stdout.flush() 
+
+      if key == "start":
+        appRunning = True
+        disp.fill(0)
+        disp.show()
+
+      if key == "clear":
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0, 0, width, height), outline=0, fill=0) 
+        disp.fill(0)   
+
+      if key == "drawText":
+        x = int(payload["x"])
+        y = int(payload["y"])
+        text = payload["text"]
+        # color = int(payload["color"])
+        disp.text(text, x, y, 1)
+
+      if key == "drawCircle":
+          x1 = int(payload["x"])
+          y1 = int(payload["y"])
+          radius = int(payload["radius"])
+          color = int(payload["color"])
+          disp.circle(x,y,radius,color)
+
+      if key == "drawLine":
+          x1 = int(payload["x0"])
+          y1 = int(payload["y0"])
+          x2 = int(payload["x1"])
+          y2 = int(payload["y1"])
+          color = int(payload["color"])
+          disp.line(x1,y1,x2,y2,color)
+
+      if key == "setPixel":
+          x = int(payload["x"])
+          y = int(payload["y"])
+          color = int(payload["color"])
+          disp.pixel(x,y,color)
+
+
+      if key == "render":
+        appRunning = True
+        disp.show()
+
+
+
+
+        
